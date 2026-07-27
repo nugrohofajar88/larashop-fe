38957175201bcc303210202d5d6eaf5a0291ceac
Fajar Nugroho <fajar_nugroho@smltech.co.id>
2026-07-27 10:42:30 +0700

    end debugging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\EnsureAdminSession;
-use App\Http\Middleware\DebugLoginRequest;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.session' => EnsureAdminSession::class,
         ]);
-        $middleware->prependToGroup('web', DebugLoginRequest::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
